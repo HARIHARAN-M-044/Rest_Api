92a0e9bcd54f6f5ce33fe76d13f799513be88321
Hari <108513817+HARIHARAN-M-044@users.noreply.github.com>
2023-08-25 15:20:09 +0530

    Delete Day_5/DAY_5 directory

--- Day_5/DAY_5/src/main/java/com/example/demo/Repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.BookEntity;
-
-public interface BookRepo extends JpaRepository<BookEntity, Integer>{
-
-}
